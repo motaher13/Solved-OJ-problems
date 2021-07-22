7e6d4a0665456d13b116b0ebeead811dccfce3f2
Motaher <motaher@Motahers-MacBook-Pro.local>
2021-07-22 13:10:20 +0600

    update

---
+++ LeetCode/DynamicProgramming/JumpGameII.java
@@ -0,0 +1,43 @@
+class JumpGameII {
+    // explanation: https://youtu.be/YKceaOhZi_k
+    // greedy
+
+    public int jump(int[] array) {
+        if(array.length==1) return 0;
+		int mxReach=array[0];
+		int steps=array[0];
+		int jumps=1;
+		
+		for(int i=1;i<array.length;i++){
+			mxReach=Math.max(mxReach,array[i]+i);
+			if(steps==i && i<array.length-1){
+				// no jump necessary if we reach the last position;
+				jumps++;
+				steps=mxReach;
+			}
+		}
+		return jumps;
+    }
+    
+
+    /*
+	//dp solution
+  public static int minNumberOfJumps(int[] array) {
+    int[] dp=new int[array.length];
+		return calculateJump(dp, array, 0);
+  }
+	
+	public static int calculateJump(int[] dp, int[] array, int pos){
+		if(pos>=array.length-1) return 0;
+		if(dp[pos]!=0) return dp[pos];
+		int mn=Integer.MAX_VALUE-1;
+		for(int i=1;i<=array[pos];i++){
+			mn=Math.min(mn, 1+calculateJump(dp, array, pos+i));
+		}
+
+		dp[pos]=mn;
+		return dp[pos];
+	}
+	*/
+    
+}
---
+++ geek/dp/Knapsack.java
@@ -0,0 +1,49 @@
+class Knapsack
+{ 
+    // Bottom-up
+    // Time O(n*w) space O(n)
+
+    static int knapSack(int weight, int wt[], int val[], int n){
+        int[] dp=new int[weight+1];
+
+        //doesn't work somehow
+        /*for(int i=0;i<n;i++){
+            //building each weight from old weight
+            for(int w=0;w<=weight;w++){
+                if(w-wt[i]==0 || (w-wt[i]>0 && dp[w-wt[i]]>0)){
+                    dp[w]=Math.max(dp[w],dp[w-wt[i]]+val[i]);
+                }
+            }
+        }
+        return dp[weight];*/
+        
+        for (int i = 0; i < n; i++) {
+            for (int w = weight; w >= 0; w--) {
+     
+                if (wt[i] <= w)
+                    // finding the maximum value
+                    dp[w] = Math.max(dp[w],
+                                dp[w - wt[i]] + val[i]);
+            }
+        }
+        return dp[weight];
+    }
+
+
+
+    // Time O(n*w) space O(n*w) top-down
+    static int knapSack1(int w, int wt[], int val[], int n){
+        int[][] dp=new int[n+1][w+1];
+        return calculate1(w,wt,val,n,dp,0);
+    }
+
+    static int calculate1(int w, int wt[], int val[], int n, int[][] dp, int ind){
+        if(ind>=n || w==0) return 0;
+        if(dp[ind][w]!=0) return dp[ind][w];
+
+        int includeThis=w-wt[ind]>=0? calculate1(w-wt[ind],wt,val,n,dp,ind+1)+val[ind]:0;
+        int notInclThis=calculate1(w,wt,val,n,dp,ind+1);
+        dp[ind][w]=Math.max(includeThis,notInclThis);
+        return dp[ind][w];
+    }
+}

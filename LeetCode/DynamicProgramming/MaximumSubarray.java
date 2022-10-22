0509b7589ba8aebf7ed620405c9bc4bdb991066a
motaher13 <md.motaherhossain04@gmail.com>
2022-10-22 19:15:13 +0600

    fix

--- LeetCode/DynamicProgramming/MaximumSubarray.java
+++ LeetCode/DynamicProgramming/MaximumSubarray.java
@@ -1,24 +1,13 @@
-package LeetCode.DynamicProgramming;
-
-public class MaximumSubarray {
-
+class Solution {
     public int maxSubArray(int[] nums) {
-        if(nums.length==0) return 0;
-        int max=0;
-        int sum=0;
-        sum+=nums[0];
-        max=sum;
-        for(int i=1;i<nums.length;i++){
-            if(sum+nums[i]>nums[i]) sum+=nums[i];
-            else sum=nums[i];
-            max=Math.max(max,sum);
-        }
-        return max;
-    }
+        int sum=nums[0];
+        int mx=nums[0];
 
-    public static void main(String[] args){
-        int[] a={-3,-4,-2};
-        int k=(new MaximumSubarray()).maxSubArray(a);
-        System.out.println(k);
+        for(int i=1;i<nums.length;i++){
+            if(sum>0) sum+=nums[i];
+            else sum=nums[i];
+            mx=Math.max(mx, sum);
+        }
+        return mx;
     }
 }

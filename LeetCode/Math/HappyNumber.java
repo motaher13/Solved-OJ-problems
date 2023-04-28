363c87320d08c4d0507eeb168adac4ecbffd8bc4
motaher13 <md.motaherhossain04@gmail.com>
2023-04-28 09:54:32 +0600

    add

--- LeetCode/Math/HappyNumber.java
+++ LeetCode/Math/HappyNumber.java
@@ -2,7 +2,39 @@ package LeetCode.Math;
 
 public class HappyNumber {
 
+    // it's acutually a two pointer cycle problem. like cycle in linkedlist. so solve it that way
+
     public boolean isHappy(int n) {
+        while(true){
+            int slow=n, fast=n;
+            do{
+                slow=getsquared(slow);
+                fast=getsquared(getsquared(fast));
+            }while(slow!=fast);
+
+            return slow==1;
+        }
+    }
+
+    private int getsquared(int n){
+        int squaresum=0;
+        while(n>0){
+            int last=n%10;
+            squaresum+=last*last;
+            n/=10;
+        }
+        return squaresum;
+    }
+
+    /*
+     * What's the biggest number that could have a next value bigger than itself? Well we know it has to be less than 243243243, from the analysis we did previously. Therefore, we know that any cycles must contain numbers smaller than 243243243, as anything bigger could not be cycled back to. With such small numbers, it's not difficult to write a brute force program that finds all the cycles.
+
+If you do this, you'll find there's only one cycle: 4→16→37→58→89→145→42→20→44 \rightarrow 16 \rightarrow 37 \rightarrow 58 \rightarrow 89 \rightarrow 145 \rightarrow 42 \rightarrow 20 \rightarrow 44→16→37→58→89→145→42→20→4. All other numbers are on chains that lead into this cycle, or on chains that lead into 111.
+
+Therefore, we can just hardcode a HashSet containing these numbers, and if we ever reach one of them, then we know we're in the cycle. There's no need to keep track of where we've been previously.
+     * 
+     */
+    public boolean isHappyShortcut(int n) {
         while (true){
             int squareSum=0;
             while (n>0){

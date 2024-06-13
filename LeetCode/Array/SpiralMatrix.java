847720fe9c8bbe3ef9fa7c279ee67f22cac15610
motaher13 <md.motaherhossain04@gmail.com>
2024-06-13 11:48:19 +0600

    update

--- LeetCode/Array/SpiralMatrix.java
+++ LeetCode/Array/SpiralMatrix.java
@@ -1,59 +1,35 @@
-package LeetCode.Array;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class SpiralMatrix {
-
+class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> out=new ArrayList<>();
-        if(matrix.length==0) return out;
-        
-        int left=0, right=matrix[0].length-1;
-        int top=0, bottom=matrix.length-1;
-        int i=0, j=0;
-        
-        while(left<=right && top<=bottom){
-            
-            if(left<=right && top<=bottom){
-                for(j=left;j<=right;j++)
-                    out.add(matrix[i][j]);
-                top++;
-                j--;
-            }
-            
-            if(left<=right && top<=bottom){
-                for(i=top;i<=bottom;i++)
-                    out.add(matrix[i][j]);
-                right--;
-                i--;
-            }
-            
-            if(left<=right && top<=bottom){
-                for(j=right;j>=left;j--)
-                    out.add(matrix[i][j]);
-                bottom--;
-                j++;
-            }
-            
-            if(left<=right && top<=bottom){
-                for(i=bottom;i>=top;i--)
-                    out.add(matrix[i][j]);
-                left++;
-                i++;
-            }
+      int top = 0, bottom = matrix.length - 1, left = 0, right = matrix[0].length - 1;
+      List<Integer> out = new ArrayList<>();
+
+      while (top <= bottom && left <= right) {
+
+        if (top <= bottom && left <= right) {
+          for (int j = left; j <= right; j++)
+            out.add(matrix[top][j]);
+          top++;
         }
-        return out;
+
+        if (top <= bottom && left <= right) {
+          for (int i = top; i <= bottom; i++)
+            out.add(matrix[i][right]);
+          right--;
+        }
+
+        if (top <= bottom && left <= right) {
+          for (int j = right; j >= left; j--)
+            out.add(matrix[bottom][j]);
+          bottom--;
+        }
+
+        if (top <= bottom && left <= right) {
+          for (int i = bottom; i >= top; i--)
+            out.add(matrix[i][left]);
+          left++;
+        }
+      }
+
+      return out;
     }
-
-    public static void main(String[] args) {
-        int[][] a={{1,2},{3,4}};
-
-        /*a= new int[][]{{1, 2, 3,4},
-                {5, 6,7,8},
-                {9,10,11,12}};*/
-
-        List<Integer> k=(new SpiralMatrix()).spiralOrder(a);
-        System.out.println(k.toString());
-    }
-}
+  }
--- LeetCode/Others/MajorityElement.java
+++ LeetCode/Others/MajorityElement.java
@@ -18,6 +18,26 @@ public class MajorityElement {
         return maxn;
     }
 
+    public int best(int[] nums) {
+        // https://leetcode.com/problems/majority-element/solutions/3676530/3-method-s-beats-100-c-java-python-beginner-friendly/
+        // moores voing algorithom
+        /*
+           when the vote cound zero, we take the current number as candidate.
+           and we iterate the whole array, as the candidate occurs more than half time, 
+           it outweighs all other  numbers
+        */
+       
+        int count=0, candidate=0;
+        
+        for(int number:nums){
+            if(count==0) candidate=number;
+            
+            if(number==candidate) count++;
+            else count--;
+        }
+        return candidate;
+   }
+
     public static void main(String[] args) {
         int[] a={1,2,3,4,4,4,4};
         System.out.println((new MajorityElement()).majorityElement(a));
